5efc4c9f8bfc4ba4b499571dc87663bcb20afacb
Pedro <115540173+ZeToMane@users.noreply.github.com>
2024-12-02 17:19:28 +0100

    feat: added @supabase/ssr and @supabase configs for auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { isBrowser } from '@supabase/ssr'
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase", "@pinia/nuxt"],
+
   supabase: {
     redirect: false,
+    redirectOptions: {
+      login: '/',
+      callback: '/admin',
+      include: undefined,
+      exclude: [],
+      cookieRedirect: false,
+    },
+    clientOptions: {
+      auth: {
+        flowType: 'pkce', // Recommended for modern, secure authentication flows
+        autoRefreshToken: isBrowser(), // Automatically refresh tokens in the browser
+        detectSessionInUrl: isBrowser(), // Detect session changes in the browser URL
+        persistSession: true, // Enable session persistence
+      },
+    },
+    cookieOptions: {
+      secure: process.env.NODE_ENV === 'production',
+    },
   },
 });
