47960674ffb75c3c905516e00fbc043cf08a9924
ccadran <clar.cadran@gmail.com>
2024-12-06 12:23:43 +0100

    fix(nuxthub): add nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,28 @@
-import { isBrowser } from '@supabase/ssr'
+import { isBrowser } from "@supabase/ssr";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase", "@pinia/nuxt"],
+  modules: ["@nuxtjs/supabase", "@pinia/nuxt", "@nuxthub/core"],
   css: ["~/assets/styles/index.scss"],
   supabase: {
     redirect: false,
     redirectOptions: {
-      login: '/',
-      callback: '/admin',
+      login: "/",
+      callback: "/admin",
       include: undefined,
       exclude: [],
       cookieRedirect: false,
     },
     clientOptions: {
       auth: {
-        flowType: 'pkce', // Recommended for modern, secure authentication flows
+        flowType: "pkce", // Recommended for modern, secure authentication flows
         autoRefreshToken: isBrowser(), // Automatically refresh tokens in the browser
         detectSessionInUrl: isBrowser(), // Detect session changes in the browser URL
         persistSession: true, // Enable session persistence
       },
     },
     cookieOptions: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === "production",
     },
   },
 });
